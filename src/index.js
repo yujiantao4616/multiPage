74313e7fd1a3ec7f2ccb78316b8e443cb4983fa7
yujiantao <yujiantao@mysteel.com>
2020-04-23 15:28:20 +0800

    框架搭建

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 //引入 公共样式
-// import './localCss/indexLatest.css';
-// import './ued-components-latest/theme-blue/ued-components.css';
-// import './style/custom.css';
-// import './localCss/minireset.css';
-// import './localCss/mdmComponents.css';
-// import './localCss/namingRuleMgr.css';
+import './localCss/indexLatest.css';
+import './ued-components-latest/theme-blue/ued-components.css';
+import './style/custom.css';
+import './localCss/minireset.css';
+import './localCss/mdmComponents.css';
+import './localCss/namingRuleMgr.css';
